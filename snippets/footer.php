f8c91bd572f718e9dc2d0aee7ffb413fafc109cb
Emilee <hatch.emilee.cm@gmail.com>
2023-09-22 09:24:40 -0600

    created a git repository and pushed to the repo

---
+++ snippets/footer.php
@@ -0,0 +1,4 @@
+<hr>
+<p>&copy;2023 PHP Motors, All rights reserved.</p>
+<p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+<p>Last Updated: 18 September, 2023</p>
---
+++ snippets/header.php
@@ -0,0 +1,3 @@
+<div id="logoDiv"><img id="logo" src="/phpmotors/images/site/logo.png" alt="Company Logo"></div>
+
+<div id="login"><a href="/">My Account</a></div>
