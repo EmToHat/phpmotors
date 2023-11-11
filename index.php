4f3b928de4d8faf456db708ce29b5a27c82dc37d
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-11-10 20:51:33 -0700

    enhancement 5

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <?php
-// This is the main controller
-$action = filter_input(INPUT_POST, 'action');
- if ($action == NULL){
-  $action = filter_input(INPUT_GET, 'action');
- } 
- 
-switch ($action){
- case 'template':
-    include 'view/template.php';
-  break;
- default:
-  include 'view/home.php';
-}
+    // This is the main controller
+
+
+    // Get the database connection file
+    require_once 'library/connections.php';
+
+    // Functions Library
+    require_once 'library/functions.php';
+
+    // Get the PHP Motors model for use as needed
+    require_once 'model/main-model.php';
+
+    // Get the array of classifications
+	$classifications = getClassifications();
+
+    //var_dump($classifications);
+	//exit;
+
+
+    // Build the navigation menu
+    $navList = buildNavMenu($classifications);
+
+    //echo $navList;
+    //exit;
+
+    $action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL){
+        $action = filter_input(INPUT_GET, 'action');
+    }
+
+    switch ($action) {
+        case 'template':
+            include 'view/template.php';
+        break;
+        default:
+            include 'view/home.php';
+        break;
+    }
 ?>
